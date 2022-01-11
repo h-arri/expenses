51066c93dca2274c89669c788d789d9737e386ab
Hari <hariharan.2309.s@gmail.com>
2022-01-11 22:51:14 +0100

    Added base styles and fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,18 @@
+import { css } from '@emotion/css'
 import * as React from 'react'
 import { render } from 'react-dom'
 import App from './App'
 
-render(<App />, document.body)
+const styles = css`
+    margin: 0;
+    min-height: 100vh;
+    // font-family: -apple-system, BlinkMacSystemFont, 'Sarala', 'PT Mono', 'Laila', 'Segoe UI', 'Roboto', 'Oxygen',
+    //     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', monospace, sans-serif;
+    font-family: 'Alegreya Sans SC', sans-serif;
+    background: #dad1ee;
+    overflow: hidden;
+`
+const body = document.getElementById('root')?.parentElement
+body?.classList?.add(styles)
+
+render(<App />, document.getElementById('root'))
